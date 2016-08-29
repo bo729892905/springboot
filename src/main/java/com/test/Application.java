8140b7967d78a9e5a8a7e8154a3586f8127b7529
ren.xiaobo <ren.xiaobo@jyall.com>
2016-08-29 12:48:00 +0800

    add eureka

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,6 +17,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * Created by ren.xiaobo on 2016/8/16.
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 @ComponentScan(basePackages = {"com.test"})
 @MapperScan("com.test.mapper")
 @ServletComponentScan(basePackages = {"com.test.config"})
